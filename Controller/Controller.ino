097beb441c2c8fff6ab6d6e0e87bd5761ef568db
root <root@dev>
2017-12-31 22:33:54 +0100

    Several Improvements:
    - added more than 1 sensor
    - split onOff switch in two different switch: on and off

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -1,6 +1,24 @@
+/*
+  Copyright (c) 2017 Andrea Pilo.  All right reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
 #include "Arduino.h"
 #include <Wire.h>
 #include <Blinker.h>
+#include <LiquidCrystal_I2C.h>
 
 enum State {
 	STANDBY, ALERTING, RUNNING, DAMAGE
@@ -9,8 +27,13 @@ enum State {
 /**
  * input pins
  */
-#define SENSOR_PIN 10
-#define ON_OFF_PIN 11
+#define SENSOR_PIN_1 10
+#define SENSOR_PIN_2 11
+#define SENSOR_PIN_3 12
+#define SENSOR_PIN_4 14
+
+#define ON_PIN 8
+#define OFF_PIN 8
 
 /**
  * out pins
@@ -27,52 +50,31 @@ unsigned int STARTING_DELAY = 1000;
  */
 State state = STANDBY;
 
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
 unsigned long int alertingMillis;
-unsigned long startingMillis;
 unsigned long runningMillis;
 unsigned int alertCount = 0;
 unsigned int MAX_ALERT = 3;
-boolean onOffPinOld = true;
+boolean offPinOld = true;
+boolean onPinOld = true;
 
 Blinker blinker(LED_PIN, 1000, 1000);
-
-/*
- * Send the sensor's state to the bus
- */
-void onRequest() {
-	String sensorState;
-	if (state == ALERTING) {
-		sensorState = "X11111111X";
-	} else {
-		sensorState = "X00000000X";
-	}
-	Wire.print(sensorState);
-}
 
 void setup() {
 	Serial.begin(9600);
 
 	blinker.begin();
 
-	Wire.begin(1);
-	Wire.onRequest(onRequest);
-	Wire.onReceive(onReceive);
-
 	pinMode(LED_PIN, OUTPUT);
-	pinMode(ON_OFF_PIN, INPUT_PULLUP);
-	pinMode(SENSOR_PIN, INPUT_PULLUP);
+	pinMode(ON_PIN, INPUT_PULLUP);
+	pinMode(OFF_PIN, INPUT_PULLUP);
+	pinMode(SENSOR_PIN_1, INPUT_PULLUP);
+	pinMode(SENSOR_PIN_2, INPUT_PULLUP);
+	pinMode(SENSOR_PIN_3, INPUT_PULLUP);
+	pinMode(SENSOR_PIN_4, INPUT_PULLUP);
 
-	Serial.println("System booting on state: STANDBY");
-}
-
-/**
- * function that executes whenever data is received from master
- * this function is registered as an event, see setup()
- */
-void onReceive(int howMany) {
-	if (Wire.available()) {
-		//state = Wire.readString().toInt();
-	}
+	info("System booting ...");
 }
 
 void loop() {
@@ -92,76 +94,84 @@ void loop() {
 		damage();
 		break;
 	default:
-		Serial.println("Unknown State: " + state);
+		info("Unknown State: " + state);
 		state = DAMAGE;
 		break;
 	}
 }
 
 void changeState(State s) {
-	Serial.println(state + " -> " + s);
+	info("changeState to " + s);
 	state = s;
 }
 
+void info(char * msg) {
+	Serial.println("state " + state + " - " + msg);
+}
+
+/**
+ * Should be read the onoff switch:
+ * if it is ON the next state will be STANDBY
+ */
 void damage() {
-//
-// Should be read the onoff switch:
-// if it is ON the next state will be STANDBY
-//
-	boolean onOffPin = digitalRead(ON_OFF_PIN);
-	if (onOffPinOld && !onOffPin) {
+	boolean offPin = digitalRead(OFF_PIN);
+	if (offPinOld && !offPin) {
 		changeState(STANDBY);
+	} else {
+		info("Error!");
 	}
-	onOffPinOld = onOffPin;
+	offPinOld = offPin;
 	blinker.newDuration(5000, 500);
 }
 
+/*
+ * Should be read the onoff switch:
+ * if it is OFF the next state will be STANDBY
+ * Should be read the onoff switch:
+ *  if it is ON the next state will be ALERTING
+ */
 void running() {
-//
-// Should be read the onoff switch:
-// if it is OFF the next state will be STANDBY
-//
 
-	boolean onOffPin = digitalRead(ON_OFF_PIN);
-	if (onOffPinOld && !onOffPin) {
+	boolean offPin = digitalRead(OFF_PIN);
+	if (offPinOld && !offPin) {
 		changeState(STANDBY);
+	} else if (millis() - runningMillis < 10000) {
+		info("Running since less than 10s.");
 	}
-
-//
-// Should be read the onoff switch:
-// if it is ON the next state will be ALERTING
-//
-	else if (digitalRead(SENSOR_PIN) == LOW) {
+	else if (digitalRead(SENSOR_PIN_1) == LOW) {
 		changeState(ALERTING);
 		alertingMillis = millis();
 	}
-	onOffPinOld = onOffPin;
+	offPinOld = offPin;
 	blinker.newDuration(2000, 500);
 }
 
+/*
+* Should be read the on-off switch:
+* if it is ON the next state will be RUNNING
+*/
 void standby() {
-//
-// Should be read the on-off switch:
-// if it is ON the next state will be RUNNING
-//
-	boolean onOffPin = digitalRead(ON_OFF_PIN);
-	if (onOffPinOld && !onOffPin) {
+	boolean onPin = digitalRead(ON_PIN);
+	if (onPinOld && !onPin) {
 		changeState(RUNNING);
 	}
-	onOffPinOld = onOffPin;
+	onPinOld = onPin;
 	blinker.newDuration(500, 2000);
 }
 
+/*
+* Should be read the on-off switch:
+* if it is OFF the next state will be STANDBY
+*/
 void alerting() {
-//
-// Should be read the on-off switch:
-// if it is OFF the next state will be STANDBY
-//
 
-	boolean onOffPin = digitalRead(ON_OFF_PIN);
-	if (onOffPinOld && !onOffPin) {
+	boolean offPin = digitalRead(OFF_PIN);
+	if (offPinOld && !offPin) {
 		changeState(STANDBY);
+	} else if (millis() - alertingMillis > 60000) {
+		changeState(RUNNING);
+		runningMillis = millis();
 	}
-	onOffPinOld = onOffPin;
+	offPinOld = offPin;
 	blinker.newDuration(500, 500);
 }
